a23fa8eeadcfa1f65d909aa2fa640abe6395394a
Joel B. Mohler <joel@kiwistrawberry.us>
2021-08-03 22:12:55 -0400

    a map in text mode

---
+++ pmlib/core.py
@@ -0,0 +1,23 @@
+
+
+# +-----------------------+
+# |oooooo*ooooooooo*oooooo|
+# oo+---------o---------+oo
+# |o|o*ooooooooooooooo*o|o|
+# |o+----o+-------+o----+o|
+# |ooooooooooooooooooooooo|
+# |o+----o|o+---+o|o----+o|
+# |o|o*ooo|o|xxx|o|ooo*o|o|
+# |o+o----+o+-x-+o+----o+o|
+# |ooooooo|ooo@ooo|ooooooo|
+# |o+----o+o-----o+o----+o|
+# |o|o*ooooooooooooooo*o|o|
+# oo+---------o---------+oo
+# |oooooo*ooooooooo*oooooo|
+# +-----------------------+
+
+class PacmanBoard:
+    WIDTH = 11 * 25
+    HEIGHT = 11 * 15
+
+
