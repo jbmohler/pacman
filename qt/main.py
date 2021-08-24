9f648f77ad78c79c832767f73b10e8ab6cbe5c89
Joel B. Mohler <joel@kiwistrawberry.us>
2021-08-24 06:28:42 -0400

    show all the wall types in the POC

--- qt/main.py
+++ qt/main.py
@@ -12,23 +12,55 @@ class PacmanWidget(QtWidgets.QWidget):
         self.setMinimumHeight(32 * 15)
         self.setMinimumWidth(32 * 25)
 
+        self._pixmaps = {}
+
+    def cached_wall_pixmap(self, bits):
+        if bits not in self._pixmaps:
+            pngfile = os.path.join(ARTDIR, f"wall-{bits}.png")
+            self._pixmaps[bits] = QtGui.QPixmap(pngfile)
+
+        return self._pixmaps[bits]
+
     def paintEvent(self, e):
-        painter = QtWidgets.QPainter(self)
-
-        painter.begin(self)
-
-        pix0101 = QtGui.QPixmap(os.path.join(ARTDIR, "wall-0101.png"))
-        pix0110 = QtGui.QPixmap(os.path.join(ARTDIR, "wall-0110.png"))
-        pix0011 = QtGui.QPixmap(os.path.join(ARTDIR, "wall-0011.png"))
+        painter = QtGui.QPainter(self)
 
         def paint_cell(cx, cy, pixmap):
-            painter.drawPixmap(cx * 32, cy * 32, (cx + 1) * 32, (cy + 1) * 32, pix0110)
+            painter.drawPixmap(cx * 32, cy * 32, 32, 32, pixmap)
 
-        paint_cell(1, 2, pix0110)
-        paint_cell(1, 3, pix0101)
-        paint_cell(1, 4, pix0011)
+        paint_cell(2, 2, self.cached_wall_pixmap("0110"))
+        paint_cell(3, 2, self.cached_wall_pixmap("0101"))
+        paint_cell(4, 2, self.cached_wall_pixmap("0111"))
+        paint_cell(5, 2, self.cached_wall_pixmap("0101"))
+        paint_cell(6, 2, self.cached_wall_pixmap("0011"))
 
-        painter.end()
+        paint_cell(2, 3, self.cached_wall_pixmap("1010"))
+        paint_cell(4, 3, self.cached_wall_pixmap("1010"))
+        paint_cell(6, 3, self.cached_wall_pixmap("1010"))
+
+        paint_cell(2, 4, self.cached_wall_pixmap("1110"))
+        paint_cell(3, 4, self.cached_wall_pixmap("0101"))
+        paint_cell(4, 4, self.cached_wall_pixmap("1111"))
+        paint_cell(5, 4, self.cached_wall_pixmap("0101"))
+        paint_cell(6, 4, self.cached_wall_pixmap("1011"))
+
+        paint_cell(2, 5, self.cached_wall_pixmap("1010"))
+        paint_cell(4, 5, self.cached_wall_pixmap("1010"))
+        paint_cell(6, 5, self.cached_wall_pixmap("1010"))
+
+        paint_cell(2, 6, self.cached_wall_pixmap("1100"))
+        paint_cell(3, 6, self.cached_wall_pixmap("0101"))
+        paint_cell(4, 6, self.cached_wall_pixmap("1101"))
+        paint_cell(5, 6, self.cached_wall_pixmap("0101"))
+        paint_cell(6, 6, self.cached_wall_pixmap("1001"))
+
+        paint_cell(3, 5, self.cached_wall_pixmap("0000"))
+        paint_cell(5, 3, self.cached_wall_pixmap("0000"))
+
+        paint_cell(8, 3, self.cached_wall_pixmap("0010"))
+        paint_cell(7, 4, self.cached_wall_pixmap("0100"))
+        paint_cell(8, 4, self.cached_wall_pixmap("1111"))
+        paint_cell(9, 4, self.cached_wall_pixmap("0001"))
+        paint_cell(8, 5, self.cached_wall_pixmap("1000"))
 
 
 class MainWindow(QtWidgets.QMainWindow):
@@ -50,4 +82,4 @@ if __name__ == "__main__":
     app.main = MainWindow()
     app.main.show()
 
-    app.exec_()
+    app.exec()
